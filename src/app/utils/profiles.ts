b6e2a6697fcd1f32d04c74238e24d15072bc6483
shettycs <135249208+shettycs@users.noreply.github.com>
2024-09-28 12:34:50 +0530

    charishma

--- src/app/utils/profiles.ts
+++ src/app/utils/profiles.ts
@@ -20,4 +20,9 @@ export const profiles: ProfileProps[] = [
     name: "Satwik R Prabhu",
     quote: "I know where you live 🌝",
   },
+  {
+    username: "charishma",
+    name: "Charishma Shetty",
+    quote: "charishma cool",
+  },
 ];

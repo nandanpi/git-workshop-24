b0d15395887cdc8a21564650951af1be8f71e875
Neelima <neelimabhaktha01@gmail.com>
2024-09-28 12:34:03 +0530

    neelima

--- src/app/utils/profiles.ts
+++ src/app/utils/profiles.ts
@@ -20,4 +20,9 @@ export const profiles: ProfileProps[] = [
     name: "Satwik R Prabhu",
     quote: "I know where you live 🌝",
   },
+  {
+    username: "neelima bhaktha",
+    name: "Neelima bhaktha",
+    quote: "Nandan likes Cartoon Network! so childish",
+  },
 ];

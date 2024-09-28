40e1bd84434a16205784facb00b38e56f1ddc7d8
sanidhya <sanidhyadatt26@gmail.com>
2024-09-28 12:18:45 +0530

    changes1

--- src/app/utils/profiles.ts
+++ src/app/utils/profiles.ts
@@ -20,4 +20,9 @@ export const profiles: ProfileProps[] = [
     name: "Satwik R Prabhu",
     quote: "I know where you live 🌝",
   },
-];
+  {
+    username: "sanidhyadatt",
+    name: "Sanidhya Datt",
+    quote: "Set your Heart Ablaze! ",
+  },
+]

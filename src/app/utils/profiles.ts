32d75f68c6a7e4800e2ecde35d3fa12419724613
Your Name <saurabh>
2024-09-28 12:32:54 +0530

    add Saurabh-acharyaa

--- src/app/utils/profiles.ts
+++ src/app/utils/profiles.ts
@@ -20,4 +20,9 @@ export const profiles: ProfileProps[] = [
     name: "Satwik R Prabhu",
     quote: "I know where you live 🌝",
   },
+  {
+    username: "Saurabh-acharyaa",
+    name: "Saurabh Acharya",
+    quote: "Good For You 🌝",
+  },
 ];

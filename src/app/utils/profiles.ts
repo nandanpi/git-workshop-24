5a97361a2440bedb4ad3b897075fd03606e73b04
Ayush-raii <ayushurai3@gmail.com>
2024-09-28 12:31:52 +0530

    add: Ayush-Raii entry

--- src/app/utils/profiles.ts
+++ src/app/utils/profiles.ts
@@ -20,4 +20,9 @@ export const profiles: ProfileProps[] = [
     name: "Satwik R Prabhu",
     quote: "I know where you live 🌝",
   },
+  {
+   username: "Ayush-raii",
+   name: "AYUSH RAI",
+   quote: "president",
+  },
 ];

ad8f66f5af8476bbc5bd0bf10f97fb5fe1f58a0f
Prableen <prableenkour033@gmail.com>
2024-09-28 12:34:19 +0530

    Prableen

--- src/app/utils/profiles.ts
+++ src/app/utils/profiles.ts
@@ -20,4 +20,9 @@ export const profiles: ProfileProps[] = [
     name: "Satwik R Prabhu",
     quote: "I know where you live 🌝",
   },
+  {
+    username: "Prableen",
+    name: "Prableen Kour",
+    quote: "Nandan likes Cartoon Network! so childish",
+  },
 ];

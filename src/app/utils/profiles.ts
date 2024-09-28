e2191603d3392c848543e9742b5c0218a56f68d5
TrishalHegde <trishalhegde@gmail.com>
2024-09-28 12:22:51 +0530

    add:TrishalHegde entry

--- src/app/utils/profiles.ts
+++ src/app/utils/profiles.ts
@@ -20,4 +20,9 @@ export const profiles: ProfileProps[] = [
     name: "Satwik R Prabhu",
     quote: "I know where you live 🌝",
   },
+  {
+    username: "TrishalHegde",
+    name: "trishal hegde",
+    quote: "likho 98...",
+  },
 ];

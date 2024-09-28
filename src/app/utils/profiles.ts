b033fa4ef301b8d64f842e7f3b491242748b65c3
ikey-droid <da7533699@gmail.com>
2024-09-28 00:35:51 -0700

    feature

--- src/app/utils/profiles.ts
+++ src/app/utils/profiles.ts
@@ -20,4 +20,9 @@ export const profiles: ProfileProps[] = [
     name: "Satwik R Prabhu",
     quote: "I know where you live 🌝",
   },
+  {
+    username: "nandanpi",
+    name: "Nandan Pai",
+    quote: "Nandan likes Cartoon Network! so childish",
+  },
 ];

934cff06566c270487b7a608ea7ea23fa9cf648d
Avyakta-dev <avyakta061@gmail.com>
2024-09-28 12:24:41 +0530

    my commit

--- src/app/utils/profiles.ts
+++ src/app/utils/profiles.ts
@@ -20,4 +20,9 @@ export const profiles: ProfileProps[] = [
     name: "Satwik R Prabhu",
     quote: "I know where you live 🌝",
   },
+  {
+    username: "avyakta-dev",
+    name: "Avyakta S",
+    quote: "I love learning new skills",
+  },
 ];

a11f80f2f89d22b999749beb314fe0aec651d977
Mithun095 <mithunmallya095@gmail.com>
2024-09-28 12:22:24 +0530

    add : its myth and its my entry

--- src/app/utils/profiles.ts
+++ src/app/utils/profiles.ts
@@ -20,4 +20,9 @@ export const profiles: ProfileProps[] = [
     name: "Satwik R Prabhu",
     quote: "I know where you live 🌝",
   },
+  {
+    username: "Mithun095",
+    name: "V Mithun Mallya",
+    quote: "who is uniq_myth ?? ",
+  },
 ];

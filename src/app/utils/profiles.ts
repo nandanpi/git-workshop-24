c7aa6e236136f1f170e1f0a2dbf4e79706fd00ff
Light-2406 <ft.light24@gmail.com>
2024-09-28 12:22:47 +0530

    new commit

--- src/app/utils/profiles.ts
+++ src/app/utils/profiles.ts
@@ -20,4 +20,9 @@ export const profiles: ProfileProps[] = [
     name: "Satwik R Prabhu",
     quote: "I know where you live 🌝",
   },
+   
+  { username: "Light-2406",
+    name: "Jyothii M",
+    quote: "I'm confused' 🌝",
+},
 ];

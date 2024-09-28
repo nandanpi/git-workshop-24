806369dfe0f3264e2e481098963e21e621e47b06
adithyasn11 <adithyasn2487@gamil.com>
2024-09-28 12:34:05 +0530

    adithya

--- src/app/utils/profiles.ts
+++ src/app/utils/profiles.ts
@@ -20,4 +20,9 @@ export const profiles: ProfileProps[] = [
     name: "Satwik R Prabhu",
     quote: "I know where you live 🌝",
   },
+  {
+    username: "adithya",
+    name: "adithya s nayak",
+    quote: "adithya likes Cartoon Network! so childish",
+  },
 ];

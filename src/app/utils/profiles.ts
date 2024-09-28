c8447b5282fba101c62f45b0c755315804a59e92
Keerthan <kkeerthan2005@gmail.com>
2024-09-28 12:34:29 +0530

    Keerthan

--- src/app/utils/profiles.ts
+++ src/app/utils/profiles.ts
@@ -20,4 +20,9 @@ export const profiles: ProfileProps[] = [
     name: "Satwik R Prabhu",
     quote: "I know where you live 🌝",
   },
+  {
+    username: "keetha1011",
+    name: "Keerthan",
+    quote: "KrashKanter",
+  },
 ];

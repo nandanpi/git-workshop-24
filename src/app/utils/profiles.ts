7197381283e68270fad189971522ca1710097a78
Sahana-pixel <sahanaacharya85@gmail.com>
2024-09-28 12:48:15 +0530

    add sahana-pixel

--- src/app/utils/profiles.ts
+++ src/app/utils/profiles.ts
@@ -20,4 +20,9 @@ export const profiles: ProfileProps[] = [
     name: "Satwik R Prabhu",
     quote: "I know where you live 🌝",
   },
+  {
+    username: "Sahana-pixel",
+    name: "Sahana s acharya",
+    quote: "I love coding ",
+  },
 ];

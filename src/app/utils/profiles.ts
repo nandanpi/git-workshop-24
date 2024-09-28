97f976b1736e4d6ecebc3f8edee8fd38a73a7efc
Nishmitha <shettynishmitha134@gmail.com>
2024-09-28 12:23:38 +0530

    Nishmitha2719 entry

--- src/app/utils/profiles.ts
+++ src/app/utils/profiles.ts
@@ -20,4 +20,9 @@ export const profiles: ProfileProps[] = [
     name: "Satwik R Prabhu",
     quote: "I know where you live 🌝",
   },
+  {
+    username: "Nishmitha2719",
+    name: "Nishmitha",
+    quote: "I know where you live 🌝",
+  },
 ];
